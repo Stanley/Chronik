dcf960e879ba0a310c067c341a66862d0975b785
Stanisław Wasiutyński <staszek.wasiutynski@gmail.com>
2011-07-30 14:52:35 +0000

    + all holidays label support; all tests green

--- lib/chronik/chronik.rb
+++ lib/chronik/chronik.rb
@@ -1,7 +1,16 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
 module Chronik
   class << self
+
     private
 
+    # The Easter Sunday in the given year
+    #
+    # year - [integer]
+    #
+    # Returs Date
     def easter(year)
       c = year/100
       n = year-19*(year/19)
@@ -18,29 +27,36 @@ module Chronik
       Date.new year, month, day
     end
 
-    def easter_dates(holidays=[0])
-      lambda do |range|
-        (range.first.year..range.last.year).map do |year|
-          holidays.map{|offset| easter(year) + offset}
-        end.flatten.grep(range)
-      end
+    def easter_dates(holidays=0)
+      holidays = [holidays] unless holidays.respond_to?(:map)
+      dates(lambda{|year| holidays.map{|offset| easter(year) + offset} })
     end
 
     def fixed_dates(month, day)
+      dates(lambda{|year| p year,month,day; Date.new(year, month, day) })
+    end
+
+    def dates(days)
       lambda do |range|
         (range.first.year..range.last.year).map do |year|
-          Date.new(year, month, day)
-        end.grep(range)
+          days.call(year)
+        end.flatten.grep(range)
       end
     end
   end
 
+  SUGAR = [:dni, :i, :w]
+
+  ALL_HOLIDAYS = [:'święta', :wolne]
+
   HOLIDAYS = {
     [:wielkanoc, :wielkanocy] => easter_dates(0..1),
-    [:sylwester, :sylwestra] => fixed_dates(31,12)
+    [:wielkanocny] => easter_dates(1),
+    [:sylwester, :sylwestra] => fixed_dates(12,31)
   }
 
   WEEKDAYS = {
+    [:*, :'tydzień'] => (0..6).to_a,
     [:powszedni, :powszednie] => (1..5).to_a,
     [:weekend, :weekendy] => [0,6],
     [:sobota, :soboty] => [6],
--- lib/chronik/label.rb
+++ lib/chronik/label.rb
@@ -1,14 +1,21 @@
+require 'unicode'
+
 module Chronik
   #
   class Label
     def initialize(text)
-      @tokens = text.split.map{|word| word.downcase.to_sym}
+      @tokens = text.split.map{|word| Unicode.downcase(word).to_sym} - SUGAR
     end
     
     # Returns
     def holidays(range=(Date.today..Date.today.next_month))
       output = []
       @tokens.each do |token|
+        if ALL_HOLIDAYS.include?(token)
+          return HOLIDAYS.values.map do |dates|
+            dates.call(range)
+          end.flatten.uniq
+        end
         HOLIDAYS.each_pair do |labels,dates|
           if labels.include?(token)
             output.concat(dates.call(range))
